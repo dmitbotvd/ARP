968bef1dccc03630cfca6cd4b5612a899a3c7905
bot <dmitbotvd@gmail.com>
2020-01-24 13:34:46 +0200

    add

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
     dots: true,
     slidesToShow: 3,
     slidesToScroll: 3,
+    appendArrows: $('.last-photo-button'),
+    prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+    nextArrow: '<button id="next" type="button" class="btn btn-juliet"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
   });
   $('.single-item').slick({
     dots: true
